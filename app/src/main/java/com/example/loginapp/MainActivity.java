bb8b0a2b815166c9db6137d368164934843f3df4
Latiful_Bari_Pappu <latifulbari2001@gmail.com>
2023-05-17 20:48:10 +0600

    day 3

--- app/src/main/java/com/example/loginapp/MainActivity.java
+++ app/src/main/java/com/example/loginapp/MainActivity.java
@@ -1,15 +1,28 @@
 package com.example.loginapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
+    ImageView googleBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
 
         TextView username=findViewById(R.id.username);
         TextView password=findViewById(R.id.password);
+        googleBtn=findViewById(R.id.googlebtn);
 
         MaterialButton loginbtn = findViewById(R.id.loginbtn);
         loginbtn.setOnClickListener(new View.OnClickListener() {
@@ -31,5 +45,39 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        gso=new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        gsc= GoogleSignIn.getClient(this,gso);
+
+        googleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
+    }
+    void signIn(){
+        Intent signInIntent=gsc.getSignInIntent();
+        startActivityForResult(signInIntent,1000);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1000) {
+
+            Task<GoogleSignInAccount> task=GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                task.getResult(ApiException.class);
+                navigateToSecondActivity();
+            } catch (ApiException e) {
+                Toast.makeText(getApplicationContext(),"Something went wrong...",Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+    void navigateToSecondActivity(){
+        finish();
+        Intent intent=new Intent(MainActivity.this,SecondActivity.class);
+        startActivity(intent);
     }
 }
